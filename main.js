a04af491e63fdfc1abfa5b5ccf0586d5198c6715
Dora Budić <dbudic0706@gmail.com>
2021-03-25 14:57:51 +0100

    Add task on enter

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
 var app = new Vue ({
   el: '#root',
   data: {
-
+    inputText: '',
+    tasks: [
+      {
+        text: 'Call the decorator',
+        status: 'todo'
+      },
+      {
+        text: 'Go to the store',
+        status: 'todo'
+      },
+      {
+        text: 'Buy present for mum',
+        status: 'todo'
+      },
+      {
+        text: 'Do the laundry',
+        status: 'todo'
+      },
+    ],
   },
   methods: {
-
+    add: function () {
+      let obj = {
+        text: this.inputText,
+        status: 'todo'
+      }
+      this.tasks.push(obj);
+      this.inputText = '';
+    }
   }
 });
